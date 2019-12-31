ca2a2aabdccd3473557eb4ff15a977fd56cab79d
shreyanshjn <shrey.umt2017@iitr.ac.in>
2019-12-31 13:45:23 +0530

    generate certi initial code

---
+++ details.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/href-no-hash */
+// eslint-disable-next-line jsx-a11y/href-no-hash
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/href-no-hash */
+var coordiDetails = 
+    [
+        {
+            'Name': 'tester',
+            'Email': 'tester@gmail.com',
+            'Post': 'coordinator'
+        }
+    ]
